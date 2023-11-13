8ddadf3d2eaedab85bf29ef0399083e4e62ab53c
brunetto <brunetto@insa-toulouse.fr>
2023-11-13 09:34:21 +0100

    Version 13/11

---
+++ src/main/java/controller/SignalReceptionUnicastController.java
@@ -0,0 +1,65 @@
+package controller;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class SignalReceptionUnicastController  extends Thread{
+
+	//---------------------------Attributs-------------------------
+
+	private int specificPortLocal;
+	
+	//---------------------------Méthodes-------------------------
+	
+	//----------Constructeur
+	
+	public SignalReceptionUnicastController() { 				
+		
+		this.specificPortLocal = 5000;
+	}
+
+	//----------Getters
+
+	public int GetSpecificPortLocal() {
+		return this.specificPortLocal;
+	}
+	
+	
+	@Override
+    public void run() {
+        try{
+          
+        DatagramSocket socket = new DatagramSocket(specificPortLocal);
+        boolean running = true;
+        byte[] buf = new byte[256];
+
+        while (running) {
+            DatagramPacket inPacket  = new DatagramPacket(buf, buf.length);
+            socket.receive(inPacket);
+            String receivedMessage = new String(inPacket.getData(), 0, inPacket.getLength());
+
+            if (0!=0 ) {//Reception Bouton quitter
+                running = false;
+                continue;
+            }
+            else{
+
+                System.out.println("Message reçu: " + receivedMessage);
+                System.out.println("@IP source = " + inPacket.getAddress().toString());
+            }
+            
+        }
+        socket.close();  
+        System.out.println("Déconnexion");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception raised");
+        }
+    }
+	
+	
+	
+}
--- src/main/java/test/TestEnvoi.java
+++ src/main/java/test/TestEnvoi.java
@@ -1,12 +1,13 @@
 package test;
 
 import controller.SignalEnvoiBroadcastController;
+import controller.SignalEnvoiUnicastController;
 
 public class TestEnvoi {
     
     
     public static void main(String[] args){
-
+    	
     	SignalEnvoiBroadcastController sebc = new SignalEnvoiBroadcastController();
 
     	sebc.EnvoyerSignalConnexionBroadcast("PseudoZero");
@@ -16,7 +17,15 @@ public class TestEnvoi {
     	
     	sebc.EnvoyerSignalDeconnexionBroadcast();
     	
+    	/*
+    	SignalEnvoiUnicastController seuc = new SignalEnvoiUnicastController();
 
+    	seuc.EnvoyerSignalUnicast("Message 1", "127.0.0.1", 6000, 6001);
+    	
+    	seuc.EnvoyerSignalChangementPseudo("Robert");
+    	seuc.EnvoyerSignalChangementPseudo("Jean Michel");
+    	*/
+    	
 
 
 
